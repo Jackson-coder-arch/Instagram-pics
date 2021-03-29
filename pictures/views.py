a72f30a2cef21b579bdf73d77307bca230911aca
Jackson-coder-arch <jacksonikonya@gmail.com>
2021-03-29 04:48:24 +0300

    newpost html

--- pictures/views.py
+++ pictures/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect,get_object_or_404
+from django.http import HttpResponse,HttpResponseRedirect
+# from django.contrib.auth.decorators import login_required
+# from .forms import NewPostForm,
+from .models import Image_posts,Profile,Comment
 
 # Create your views here.
 def Image_posts(request):
@@ -28,6 +32,29 @@ def Image_posts(request):
 
 
 
-def image_posts(request):
+def NewPost(request):
+    user = request.user
+    files_objs = []
 
-    return render(request,'image_posts')
+    if request.method == 'POST':
+        form = request.FILES.getlist('content')
+        caption = form.cleaned_data.get('caption')
+
+        for files in files:
+            file_instance = PostFileContent(file=file, user=user)
+            file_instance.save()
+            files_objs.append(file_instance)
+
+        p,created = Post.objects.get_or_create(image_caption=image_caption, user = user)
+        p.save()
+        return redirect('home')
+
+    else:
+        
+        form = NewPostForm()
+
+        context = {
+            'form' : form,
+        }
+
+    return render(request,'newpost.html',context)
