2e56ed597ffd08bbfddcb8a1bf179323d4218369
Jackson-coder-arch <jacksonikonya@gmail.com>
2021-03-29 10:52:03 +0300

    addition of static folder

--- pictures/views.py
+++ pictures/views.py
@@ -2,31 +2,31 @@ from django.shortcuts import render, redirect,get_object_or_404
 from django.http import HttpResponse,HttpResponseRedirect
 # from django.contrib.auth.decorators import login_required
 # from .forms import NewPostForm,
-from .models import Image_posts,Profile,Comment
+from .models import Image_posts,Profile,Comment,Likes
 
 # Create your views here.
 def home(request):
-    images = Image_post.objects.all()
-    current_user = request.user
-    users = Profile.objects.all()
+    # images = Image_posts.objects.all()
+    # current_user = request.user
+    # users = Profile.objects.all()
 
-    if request.method == 'POST':
-        form = NewPostForm(request.POST, request.FIlES)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.user = request.user.profile
-            post.save()
-            return HttpResponseRedirect(request.path_info)
+    # if request.method == 'POST':
+    #     form = NewPostForm(request.POST, request.FIlES)
+    #     if form.is_valid():
+    #         post = form.save(commit=False)
+    #         post.user = request.user.profile
+    #         post.save()
+    #         return HttpResponseRedirect(request.path_info)
 
-        else:
-            form = NewPostForm()
-            params ={
-                images : images,
-                'form': form,
-                'users': users,
-            }
+    #     else:
+    #         form = NewPostForm()
+    #         params ={
+    #             images : images,
+    #             'form': form,
+    #             'users': users,
+    #         }
 
-        return render(request,'home.html', params)
+        return render(request,'home.html')
 
 
 
