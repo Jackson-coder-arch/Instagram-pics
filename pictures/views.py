bb5d0b19f90c4e7db7fb715f98cc8863be5babd0
Jackson-coder-arch <jacksonikonya@gmail.com>
2021-03-29 04:29:12 +0300

    addition of if statement

--- pictures/views.py
+++ pictures/views.py
@@ -1,11 +1,29 @@
 from django.shortcuts import render
 
 # Create your views here.
-def home(request):
-    return render(request, 'home.html')
-    # images = Image_post.objects.all()
-    # current_user = request.user
-    # users = Profile.objects.all()
+def Image_posts(request):
+    images = Image_post.objects.all()
+    current_user = request.user
+    users = Profile.objects.all()
+
+    if request.method == 'POST':
+        form = NewPostForm(request.POST, request.FIlES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user.profile
+            post.save()
+            return HttpResponseRedirect(request.path_info)
+
+            else:
+                form = NewPostForm()
+                params ={
+                    images : images,
+                    'form': form,
+                    'users': users,
+                }
+            return render(request,'home.html', params)
+
+
 
 
 
